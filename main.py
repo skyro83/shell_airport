fd2d7e9331de123ddd2e5d5ab29a78cbeb63db9d
skyro83 <100965491+skyro83@users.noreply.github.com>
2024-02-23 11:50:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,87 @@
-from db.function import get_tables, recherche, update_row, add_row, delete_row, create_table
 import sqlite3
-from db.function import update_row
-database_path = "db/pokemon.db"
-def display_all():
-    conn = sqlite3.connect(database_path)
+database_path = "pokemon.db"
+def recherche() :#database_path, table_name, column_name, value):
+    conn = sqlite3.connect("pokemon.db")
     cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM pokemon")
+    #cursor.execute(f"SELECT * FROM {table_name} WHERE {column_name} = '{value}'")
+    cursor.execute("SELECT * FROM pokemon;")
     data = cursor.fetchall()
     conn.close()
     return data
 
+import os
 
+def convert_csv(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        header = lines[0].strip().split(',')
+        data = [line.strip().split(',') for line in lines[1:]]
+    return header, data
+
+def create_table(header, table_name):
+    header = ', '.join([f'"{h}" TEXT' for h in header])
+    query = f'CREATE TABLE IF NOT EXISTS {table_name} ({header})'
+    return query
+
+def insert_data(data, table_name):
+    placeholders = ', '.join('?' * len(data[0]))
+    query = f'INSERT INTO {table_name} VALUES ({placeholders})'
+    return query, data
+
+db_file = 'pokemon.db'
+csv_file = 'pokemon.csv'
+
+if not os.path.exists(db_file):
+    con = sqlite3.connect(db_file)
+    cur = con.cursor()
+    header, data = convert_csv(csv_file)
+    cur.execute(create_table(header, 'pokemon'))
+    cur.executemany(*insert_data(data, 'pokemon'))
+    con.commit()
+    con.close()
+
+
+'''
+def add_row(database_path, table_name, values):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT INTO {table_name} VALUES ({','.join(values)})")
+    conn.commit()
+    conn.close()
+    
+def delete_row(database_path, table_name, column_name, value):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    cursor.execute(f"DELETE FROM {table_name} WHERE {column_name} = '{value}'")
+    conn.commit()
+    conn.close()
+    
+def update_row(database_path, table_name, column_name, value, new_value):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    cursor.execute(f"UPDATE {table_name} SET {column_name} = '{new_value}' WHERE {column_name} = '{value}'")
+    conn.commit()
+    conn.close()
+    
+def create_table(database_path, table_name, columns):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})")
+    conn.commit()
+    conn.close()
+'''    
+def display_all():
+    conn = sqlite3.connect(database_path)
+    print("conn : ", conn)
+    cursor = conn.cursor()
+    print("cursor : ", cursor)
+    cursor.execute("SELECT * FROM pokemon")
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+print(display_all())
+'''
 def search_item(database_path):
     table_name = input("Entrez le nom de la table: ")
     column_name = input("Entrez le nom de la colonne: ")
@@ -75,3 +146,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+'''
